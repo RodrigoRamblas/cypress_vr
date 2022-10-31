7e94bdaf93aa112d8fe80d44692cc03d73df5c16
Rodrigo Ramblas <rodrigo.ramblas@lello.com.br>
2022-10-31 19:31:14 -0300

    Criando configuração local

--- cypress/e2e/vr_com_br/vr.com.br.cy.js
+++ cypress/e2e/vr_com_br/vr.com.br.cy.js
@@ -2,17 +2,17 @@ describe('Validar o fluxo Para Você', () => {
   beforeEach('Acessar o site VR', () => {
     cy.visit('/')
   })
-  //Aceitando os Termos de Privacidade da pagina
+  //Aceitar os Termos de Privacidade
   it ('Aceitar termos de Privacidade', ()=> {
     cy.get('#lum_4028808471FC1D5D0171FE80340D6AC8_quickAcceptTerm').click();
 })
 
-//Localizando o botao "PRA VOCE" e clicando
+//Localizando o botao "PRA VOCE" e clicar
 
 it ('Navegar até a seção - pra voce', ()=> {
     cy.get(':nth-child(3) > .vr-quick-navigation__link').click();
 })
-/*
+
 //Apos clicar no botao, estou validando se realmente fui direcionado para a pagina correta (/onde-aceita.htm)
 it ('Validar se estou na pagina correta', ()=> {
     cy.url().should(
@@ -27,6 +27,6 @@ it ('Clicar no botão - Onde usar meu cartão VR', ()=> {
 
     //Aqui eu estou validando se o MAPA está visivel após clicar no botão
     cy.get('#map').should('be.visible').wait(4000);
-})*/
+})
 
 })
